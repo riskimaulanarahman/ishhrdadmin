0480bf5ffc647a9f2c644902deca3989776998d8
riski maulana rahman <riskimaulanarahman@gmail.com>
2023-01-20 16:19:20 +0800

    footer updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- begin #footer -->
 <div id="footer" class="footer">
-	&copy; 2021 SIMPEK RT | Sistem Informasi Manajemen Pelaporan Kinerja Ketua RT - <a href="https://pinday.co.id" target="_blank">Pinday</a> All Rights Reserved
+	&copy; ISH HRD - All Rights Reserved
 </div>
 <!-- end #footer -->
